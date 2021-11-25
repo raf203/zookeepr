c5e80e34cae56ae30000896c91a5c851383dd05b
Rafael Cardoso <rrafa203@gmail.com>
2021-11-24 18:21:33 -0800

    add fornt-end integration

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -112,6 +114,22 @@ function filterByQuery(query, animalsArray) {
       const animal = createNewAnimal(req.body, animals);
       res.json(animal);
     }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
   app.listen(PORT, () => {
